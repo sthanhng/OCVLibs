bd8d52bd409d9561181f061104c4202b8c6dc193
ThanhNS <sthanhng@gmail.com>
2018-10-20 23:35:49 +0700

    create video_utils.py, add FPS

---
+++ utils/video_utils.py
@@ -0,0 +1,48 @@
+# *******************************************************************
+#
+# Author : Thanh Nguyen, 2018
+# Email  : sthanhng@gmail.com
+# Github : https://github.com/sthanhng
+#
+# A collection of OpenCV libraries
+#
+# Description : video_utils.py
+# Contains the main code of the methods related to video capture
+# with OpenCV
+#
+# *******************************************************************
+
+
+import datetime
+
+
+class FPS:
+    def __init__(self):
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._numFrames = 0
+
+    def start(self):
+        # start the timer
+        self._start = datetime.datetime.now()
+        return self
+
+    def stop(self):
+        # stop the timer
+        self._end = datetime.datetime.now()
+
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._numFrames += 1
+
+    def elapsed(self):
+        # return the total number of seconds between the start and
+        # end interval
+        return (self._end - self._start).total_seconds()
+
+    def fps(self):
+        # compute the (approximate) frames per second
+        return self._numFrames / self.elapsed()
